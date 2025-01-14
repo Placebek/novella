9c52e51c7f29f7ea44853cbe1720f5d004376568
sakenzi <ermekulysaken8@gmail.com>
2025-01-14 18:24:51 +0600

    created userController

---
+++ app/controllers/userController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email is already in use.' });
+        }
+
+        const hashedPassword = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        const token = jwt.sign({ id: newUser.id, email: newUser.email }, JWT_SECRET, { expiresIn: '1h'});
+
+        res.status(201).json({
+            message: 'User registered succesfully.',
+            user: {
+                id: newUser.id,
+                username: newUser.username,
+                email: newUser.email,
+            },
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error.' });
+    }
+};
+
+module.exports = {register};
