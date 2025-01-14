ee7bc46fe3704ebfa2372306d2af2a55587e07f5
sakenzi <ermekulysaken8@gmail.com>
2025-01-14 19:04:11 +0600

    created loginUser

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -40,3 +40,35 @@ exports.registerUser = async (req, res) => {
         res.status(500).json({ message: 'Server error. Please try again later.' });
     }
 };
+
+
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found.' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Invalid password.' });
+        }
+
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(200).json({
+            message: 'Login successful.',
+            user: {
+                id: user.id,
+                username: user.username,
+                email: user.email,
+            },
+            token,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error. Please try again later.' });
+    }
+};
