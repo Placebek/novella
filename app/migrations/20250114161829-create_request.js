5ded9a983fb788caf7d8e65885b3b274bbb6187b
sakenzi <ermekulysaken8@gmail.com>
2025-01-14 22:21:23 +0600

    created request migrate

---
+++ app/migrations/20250114161829-create_request.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('Requests', {
+            id: {
+                type: Sequelize.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+                allowNull: false,
+            },
+            mp3: {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            text: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            user_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Users', 
+                    key: 'id',
+                },
+                onDelete: 'CASCADE',
+            },
+            is_activate: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: true,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+            },
+        });
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('Requests');
+    },
+};
--- app/models/index.js
+++ app/models/index.js
@@ -1,10 +1,12 @@
 const sequelize = require('../config/db');
 const User = require('./userModel');
+const Request = require('./requestModel');
 
 const db = {
   sequelize,
   Sequelize: require('sequelize'),
   User,
+  Request,
 };
 
 module.exports = db;
