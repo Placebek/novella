38726fd968da578f91b4406e7bb39de975a24b86
sakenzi <ermekulysaken8@gmail.com>
2025-01-15 14:32:01 +0600

    create request pydantic model

---
+++ model/model.py
@@ -0,0 +1,19 @@
+from dataclasses import Field
+from datetime import datetime
+from typing import Optional
+from sqlalchemy import Boolean, String, Integer, ForeignKey, Column, Text, DateTime, func
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+
+class Request(Base):
+    __tablename__ = "requests"
+
+    id = Column(Integer, primary_key=True, index=True)
+    mp3 = Column(String(100), nullable=False)
+    text = Column(Text, nullable=True)
+    user_id = Column(Integer, nullable=False)
+    title = Column(String(50), nullable=True)
+    created_at: Optional[datetime] = Column(
+        DateTime(timezone=True), onupdate=func.now(), nullable=True
+    )
